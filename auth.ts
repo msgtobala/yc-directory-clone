ac6b2a8c79074027a1804d38fe0a0e593f00149d
Balaji Venkatraman <rslbalaji@gmail.com>
2025-04-23 11:16:16 +0530

    add ppr for views, check author

--- auth.ts
+++ auth.ts
@@ -1,6 +1,31 @@
 import NextAuth from 'next-auth';
 import GitHub from 'next-auth/providers/github';
+import { client } from '@/sanity/lib/client';
+import { AUTHOR_BY_GITHUB_ID_QUERY } from '@/sanity/lib/queries';
+import { writeClient } from '@/sanity/lib/write-client';
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
   providers: [GitHub],
+  callbacks: {
+    async signIn({ user, profile }) {
+      const { name, email, image } = user;
+      const existingUser = await client.fetch(AUTHOR_BY_GITHUB_ID_QUERY, {
+        id: profile?.id,
+      });
+
+      if (!existingUser) {
+        await writeClient.create({
+          _type: 'author',
+          id: profile?.id,
+          name,
+          username: profile?.login,
+          email,
+          image,
+          bio: profile?.bio,
+        });
+      }
+
+      return true;
+    },
+  },
 });
--- next.config.ts
+++ next.config.ts
@@ -11,7 +11,7 @@ const nextConfig: NextConfig = {
     ],
   },
   experimental: {
-    ppr: true,
+    ppr: "incremental",
   },
   devIndicators: {
     position: 'bottom-left',
